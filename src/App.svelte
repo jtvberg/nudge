02133577a031bfa03dac635e27ca1b7e0b8bd9e6
Joel Vandenberg <jtvberg@gmail.com>
2024-11-26 17:15:49 -0600

    logout

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 	import Nudges from '$lib/Nudges.svelte';
 	import Login from '$lib/Login.svelte';
+	import { authStore } from '$lib/stores/auth.store';
 </script>
 
 <main class="app-main app-main-dark">
+	{#if !$authStore}
+		<Login />
+	{/if}
 	<Nudges />
-	<Login />
 </main>
--- src/lib/Nudges.svelte
+++ src/lib/Nudges.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onDestroy } from 'svelte';
     import { nudgeStore } from './stores/nudges.store';
+    import { authStore } from './stores/auth.store';
     import Nudge from '$lib/Nudge.svelte';
     import Who from '$lib/Who.svelte';
 
@@ -8,7 +9,6 @@
     $: nudges = $nudgeStore;
 
     function sortNudges(nudges) {
-        console.log(nudges);
         return nudges.sort((a, b) => {
             if (a.complete !== b.complete) {
                 return a.complete ? 1 : -1;
@@ -24,6 +24,14 @@
             document.querySelectorAll('input').forEach((ele) => ele.blur());
         } catch (error) {
             console.error('Failed to add nudge:', error);
+        }
+    }
+
+    async function handleLogout() {
+        try {
+            await authStore.signOut();
+        } catch (error) {
+            console.error('Failed to logout:', error);
         }
     }
 
@@ -128,4 +136,26 @@
             {/if}
         {/if}
     </section>
+    <div class="h-6 w-6 absolute right-5 bottom-5">
+        <button
+            on:click={handleLogout}
+            class="btn-primary btn-primary-dark p-2"
+            title="Logout"
+        >
+            <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                ></path>
+            </svg>
+        </button>
+    </div>
 </section>
